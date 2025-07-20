a8a3a7af5c46a6d4f0798f8c23722617ae69b400
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-07-20 13:29:20 +0900

    PyQt5 and Data Visualization Imports

---
+++ main.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
