f163264b3930cbcb67047509b9e4a42f1a4a267c
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-07-20 13:39:37 +0900

    SimpleDashboard class created

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ from PyQt5.QtCore import Qt
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+class SimpleDashboard:
+
+    def __init__(self, root):
+        self.root = root
+
+        self.root.title("Simple Data Dashboard")
+        self.root.geometry("1000x600")
+
+        df = pd.read_csv("titanic.csv")
